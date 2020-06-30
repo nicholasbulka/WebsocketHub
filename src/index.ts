6894f780594ef32ccec79086d544ed2046e8a58b
nicholasbulka <nicholasbulka@gmail.com>
2020-06-30 16:22:49 -0400

    beginning of app separation

--- src/index.ts
+++ src/index.ts
@@ -5,36 +5,10 @@ import * as dotenv from "dotenv";
 import * as ws from 'ws';
 import expressWs from 'express-ws';
 import express from 'express';
-import winston from 'winston';
+import logger  from './util/logger';
+import socketRouter from './routes/router';
 
 dotenv.config();
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.splat(),
-    winston.format.simple()
-  ),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
 
 const eApp: express.Application = express();
 let server: http.Server|https.Server = http.createServer();
@@ -71,31 +45,4 @@ server.listen(process.env.PORT);
 const wsInstance : expressWs.Instance = expressWs(eApp, server);
 
 
-wsInstance.getWss().clients.forEach(socket => {
-    if (socket.readyState !== ws.OPEN) {
-        socket.terminate();
-        return;
-    }
-    socket.ping();
-});
-
-wsInstance.app.ws('/', (socket, req) => {
-    socket.on('message', msg => {
-      logger.log('info', "received: %s", msg);
-    });
-});
-
-const router = express.Router();
-router.ws(
-    '/:id',
-    (socket, req, next) => { next(); },
-    (socket, req, next) => {
-        socket.send(req.params.id);
-
-        socket.on('close', (code, reason) => {
-          logger.log('info', 'code %s', code);
-          logger.log('info', 'reason %s', reason);
-        });
-    }
-);
-wsInstance.app.use('/socket', router);
+wsInstance.app.use('/socket', socketRouter);
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,18 +1,21 @@
 import express from 'express';
+import expressWs from 'express-ws';
 import logger from '../util/logger'
 
-const router = express.Router();
-router.ws(
+const wsInstance : expressWs.Instance = expressWs(express());
+const socketRouter = express.Router();
+
+socketRouter.ws(
     '/:id',
     (socket, req, next) => { next(); },
     (socket, req, next) => {
         socket.send(req.params.id);
 
         socket.on('close', (code, reason) => {
-          logger.log('info', 'code %s', code);
+          logger.log('info', 'coder %s', code);
           logger.log('info', 'reason %s', reason);
         });
     }
 );
 
-export default router
+export default socketRouter
