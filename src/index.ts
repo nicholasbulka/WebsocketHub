7cb81d7bbd1d0cfa5d091e1757c236fbcb9a10eb
nicholasbulka <nicholasbulka@gmail.com>
2020-06-29 20:27:31 -0400

    typescript and express-ws

---
+++ src/index.ts
@@ -0,0 +1,86 @@
+import * as fs from 'fs';
+import * as http from 'http';
+import * as https from 'https';
+import * as dotenv from "dotenv";
+import expressWs from 'express-ws';
+import express from 'express';
+import winston from 'winston';
+
+dotenv.config();
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+const eApp: express.Application = express();
+let server: http.Server = http.createServer();
+
+const env = process.env.ENV;
+
+switch(env){
+  case 'production': {
+
+    const fc : string = process?.env?.FULLCHAIN ?? '';
+    const key : string = process?.env?.KEY ?? '';
+
+    server = https.createServer({
+      cert: fs.readFileSync(fc),
+      key: fs.readFileSync(key)
+    });
+  }
+  case 'localSSL': {
+    server = https.createServer({
+      cert: fs.readFileSync('/usr/local/etc/ssl/certs/self-signed.crt'),
+      key: fs.readFileSync('/usr/local/etc/ssl/private/self-signed.key')
+    });
+  }
+  default : {
+    server = http.createServer();
+  }
+
+}
+
+server.listen(process.env.PORT);
+
+const { app, getWss, applyTo } = expressWs(eApp, server);
+
+
+applyTo({
+    get() { return this; }
+});
+
+getWss().clients.forEach(ws => {
+    if (ws.readyState !== ws.OPEN) {
+        ws.terminate();
+        return;
+    }
+    ws.ping();
+});
+
+app.ws('/', (ws, req) => {
+    ws.on('message', msg => {
+        ws.send('got it');
+        // info: test message my string {}
+        logger.log('info', 'test message %s', 'my string');
+    });
+});
